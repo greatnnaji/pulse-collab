fd4d45cf1182d83580c4ad6e47e105d2736e0724
great nnaji <nnajiuchegreat2004@gmail.com>
2026-05-02 19:43:02 -0400

    added pageability for scale

--- src/main/java/com/pulse/repository/MessageRepository.java
+++ src/main/java/com/pulse/repository/MessageRepository.java
@@ -1,14 +1,14 @@
 package com.pulse.repository;
 
 import com.pulse.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    List<Message> findByGroupIdOrderByCreatedAtDesc(Long groupId);
+    Page<Message> findByGroupIdOrderByCreatedAtDesc(Long groupId, Pageable pageable);
 
 }
